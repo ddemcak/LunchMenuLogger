f730913916d499819ef1d26e952ab29a25f21d0a
David Demcak <ddemcak@gmail.com>
2021-07-11 23:11:16 +0200

    Add support for pieces of main dish meal.

--- Models/LunchMenuItem.cs
+++ Models/LunchMenuItem.cs
@@ -22,6 +22,7 @@ namespace LunchMenuLogger
         public DateTime Day { get; }
         public double Volume { get; }
         public double Weight { get; }
+        public int Pieces { get; }
         public string Alergens { get; }
 
 
@@ -36,12 +37,14 @@ namespace LunchMenuLogger
             // Parse line
             Weight = FindWeight(textline);
             Volume = FindVolume(textline);
+            Pieces = FindPieces(textline);
             Alergens = FindAlergens(textline);
             Price = FindPrice(textline);
-            Name = FindName(textline, Volume, Weight);
+            Name = FindName(textline, Volume, Weight, Pieces);
 
             if (Volume != 0) FoodType = FoodType.soup;
             if (Weight != 0) FoodType = FoodType.maindish;
+            if (Pieces != 0) FoodType = FoodType.maindish;
         }
 
 
@@ -66,6 +69,19 @@ namespace LunchMenuLogger
             if (match.Success)
             {
                 return double.Parse(match.Value.Replace("g", ""));
+            }
+
+            return 0;
+        }
+
+        private int FindPieces(string text)
+        {
+            Regex rgx = new Regex(@"[0-9]{1,2}ks");
+            Match match = rgx.Match(text);
+
+            if (match.Success)
+            {
+                return int.Parse(match.Value.Replace("ks", ""));
             }
 
             return 0;
@@ -97,7 +113,7 @@ namespace LunchMenuLogger
             return 0;
         }
 
-        private string FindName(string text, double vlm, double wgh)
+        private string FindName(string text, double vlm, double wgh, int pcs)
         {
             if (vlm != 0)
             {
@@ -113,14 +129,22 @@ namespace LunchMenuLogger
 
                 if (match.Success) return match.Value[1..^1].Trim();
             }
+            else if (pcs != 0)
+            {
+                Regex rgx = new Regex(@"ks.*\(");
+                Match match = rgx.Match(text);
+
+                if (match.Success) return match.Value[2..^1].Trim();
+            }
             return null;
         }
 
 
         public override string ToString()
         {
-            if(FoodType == FoodType.soup) return string.Format("{0:dd.MM.yyyy}: {1}l {2} - {3} Kč ({4})", Day, Volume, Name, Price, Alergens);
-            else return string.Format("{0:dd.MM.yyyy}: {1}g  {2} - {3} Kč ({4})", Day, Weight, Name, Price, Alergens);
+            if (FoodType == FoodType.soup) return string.Format("{0:dd.MM.yyyy}: {1}l {2} - {3} Kč ({4})", Day, Volume, Name, Price, Alergens);
+            else if (Weight != 0) return string.Format("{0:dd.MM.yyyy}: {1}g  {2} - {3} Kč ({4})", Day, Weight, Name, Price, Alergens);
+            else return string.Format("{0:dd.MM.yyyy}: {1}ks   {2} - {3} Kč ({4})", Day, Pieces, Name, Price, Alergens);
         }
 
         public string ToSqlQueryValues()
@@ -129,7 +153,8 @@ namespace LunchMenuLogger
             nfi.NumberDecimalSeparator = ".";
 
             if (FoodType == FoodType.soup) return string.Format("'{0}','{1}','{2}','{3:yyyy-MM-dd}','{4}','{5}'", Name, Price, FoodType, Day, Volume.ToString(nfi), Alergens);
-            else return string.Format("'{0}','{1}','{2}','{3:yyyy-MM-dd}','{4}','{5}'", Name, Price, FoodType, Day, Weight.ToString(nfi), Alergens);
+            else if (Weight != 0) return string.Format("'{0}','{1}','{2}','{3:yyyy-MM-dd}','{4}','{5}'", Name, Price, FoodType, Day, Weight.ToString(nfi), Alergens);
+            else return string.Format("'{0}','{1}','{2}','{3:yyyy-MM-dd}','{4}','{5}'", Name, Price, FoodType, Day, Pieces.ToString(nfi), Alergens);
         }
     }
 
